c684bff91c01566fd97dc039573a9ff92cf77a1c
Jithin P Gopal <36322329+jithinpgopal@users.noreply.github.com>
2019-12-10 15:54:17 +0530

    Create app.py

---
+++ app/app.py
@@ -0,0 +1,38 @@
+import flask
+
+app = flask.Flask(__name__)
+
+
+# Create some test data for our catalog in the form of a list of dictionaries.
+books = [
+    {'id': 0,
+     'title': 'A Fire Upon the Deep',
+     'author': 'Vernor Vinge',
+     'first_sentence': 'The coldsleep itself was dreamless.',
+     'year_published': '1992'},
+    {'id': 1,
+     'title': 'The Ones Who Walk Away From Omelas',
+     'author': 'Ursula K. Le Guin',
+     'first_sentence': 'With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.',
+     'published': '1973'},
+    {'id': 2,
+     'title': 'Dhalgren',
+     'author': 'Samuel R. Delany',
+     'first_sentence': 'to wound the autumnal city.',
+     'published': '1975'}
+]
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return '''<h1>Distant Reading Archive</h1>
+<p>A prototype API for distant reading of science fiction novels.</p>'''
+
+
+# A route to return all of the available entries in our catalog.
+@app.route('/first', methods=['GET'])
+def first():
+    return '''<h1>Container Crush Testing</h1>
+<p>I have no idea what to type here</p>'''
+
+app.run()
