49c9152590dd8882b691f6a2d76fa88dae10cb2d
Jithin P Gopal <36322329+jithinpgopal@users.noreply.github.com>
2020-01-12 23:25:18 +0530

    troubleshooting

--- app/app.py
+++ app/app.py
@@ -289,8 +289,11 @@ def prod_search():
         sub_qry["match"] = "*" + str(i) + "*"
         conj_array.append(sub_qry)
     qry = {"conjuncts": conj_array}
+    print ("query is : " + qry)
     r = requests.post(url=CB_URL, auth=cb_auth, json={"fields": ['*'], "highlight": {}, "query": qry, "size": 20})
     data = r.json()
+    print ("printing couchbase fts result :")
+    print (data)
     result_keys = []
     for i in data['hits']:
         result_keys.append(i['id'])
